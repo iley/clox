56d53fbde3c440eb4eb1a31da6fea7fc19097c86
Ilya Strukov <iley@iley.ru>
2022-10-29 11:37:15 +0300

    Hashtable optimization

--- table.c
+++ table.c
@@ -14,18 +14,18 @@ static void table_adjust_capacity(table_t* table, int new_capacity);
 
 void table_init(table_t* table) {
   table->count = 0;
-  table->capacity = 0;
+  table->capacity = -1;
   table->entries = NULL;
 }
 
 void table_free(table_t* table) {
-  FREE_ARRAY(entry_t, table->entries, table->capacity);
+  FREE_ARRAY(entry_t, table->entries, table->capacity + 1);
   table_init(table);
 }
 
 bool table_set(table_t* table, obj_string_t* key, value_t value) {
-  if (table->count + 1 > table->capacity * TABLE_MAX_LOAD) {
-    int capacity = GROW_CAPACITY(table->capacity);
+  if (table->count + 1 > (table->capacity + 1) * TABLE_MAX_LOAD) {
+    int capacity = GROW_CAPACITY(table->capacity + 1) - 1;
     table_adjust_capacity(table, capacity);
   }
 
@@ -73,7 +73,7 @@ bool table_delete(table_t* table, obj_string_t* key) {
 }
 
 void table_add_all(table_t* from, table_t* to) {
-  for (int i = 0; i < from->capacity; i++) {
+  for (int i = 0; i <= from->capacity; i++) {
     entry_t* entry = &from->entries[i];
     if (entry->key != NULL) {
       table_set(to, entry->key, entry->value);
@@ -86,7 +86,7 @@ obj_string_t* table_find_string(table_t* table, const char* chars, int length, u
     return NULL;
   }
 
-  uint32_t index = hash % table->capacity;
+  uint32_t index = hash & table->capacity;
   for (;;) {
     entry_t* entry = &table->entries[index];
     if (entry->key == NULL) {
@@ -100,12 +100,12 @@ obj_string_t* table_find_string(table_t* table, const char* chars, int length, u
       return entry->key;
     }
 
-    index = (index + 1) % table->capacity;
+    index = (index + 1) & table->capacity;
   }
 }
 
 void mark_table(table_t* table) {
-  for (int i = 0; i < table->capacity; i++) {
+  for (int i = 0; i <= table->capacity; i++) {
     entry_t* entry = &table->entries[i];
     mark_object((obj_t*)entry->key);
     mark_value(entry->value);
@@ -113,7 +113,7 @@ void mark_table(table_t* table) {
 }
 
 void table_remove_white(table_t* table) {
-  for (int i = 0; i < table->capacity; i++) {
+  for (int i = 0; i <= table->capacity; i++) {
     entry_t* entry = &table->entries[i];
     if (entry->key != NULL && !entry->key->obj.is_marked) {
       table_delete(table, entry->key);
@@ -122,7 +122,7 @@ void table_remove_white(table_t* table) {
 }
 
 static entry_t* find_entry(entry_t* entries, int capacity, obj_string_t* key) {
-  uint32_t index = key->hash % capacity;
+  uint32_t index = key->hash & capacity;
   entry_t* tombstone = NULL;
   for (;;) {
     entry_t* entry = &entries[index];
@@ -140,19 +140,19 @@ static entry_t* find_entry(entry_t* entries, int capacity, obj_string_t* key) {
       // found the entry
       return entry;
     }
-    index = (index + 1) % capacity;
+    index = (index + 1) & capacity;
   }
 }
 
 static void table_adjust_capacity(table_t* table, int new_capacity) {
-  entry_t* new_entries = ALLOCATE(entry_t, new_capacity);
-  for (int i = 0; i < new_capacity; i++) {
+  entry_t* new_entries = ALLOCATE(entry_t, new_capacity + 1);
+  for (int i = 0; i <= new_capacity; i++) {
     new_entries[i].key = NULL;
     new_entries[i].value = NIL_VAL;
   }
 
   table->count = 0;
-  for (int i = 0; i < table->capacity; i++) {
+  for (int i = 0; i <= table->capacity; i++) {
     entry_t* entry = &table->entries[i];
     if (entry->key == NULL) {
       continue;
@@ -163,7 +163,7 @@ static void table_adjust_capacity(table_t* table, int new_capacity) {
     table->count++;
   }
 
-  FREE_ARRAY(entry_t, table->entries, table->capacity);
+  FREE_ARRAY(entry_t, table->entries, table->capacity + 1);
   table->entries = new_entries;
   table->capacity = new_capacity;
 }
